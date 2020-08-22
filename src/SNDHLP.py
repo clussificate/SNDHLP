e03df6d9119fa3b6693e0d11d2470f10a7c92690
clussificate <343260443@qq.com>
2020-08-22 22:01:22 +0800

    add intial RLPM

--- src/SNDHLP.py
+++ src/SNDHLP.py
@@ -6,6 +6,7 @@
 @Desc: A branch-and-price-and-cut algorithm for service network design and hub location problem.
 """
 import numpy as np
+from pprint import pprint
 import gurobipy as gp
 from gurobipy import GRB
 import pickle
@@ -44,7 +45,7 @@ class SNDHLP:
         for a_s in self.A_s:
             ind = len(self.as_to_label)
             self.as_to_label[a_s] = ind
-            self.label_to_as[ind] = [a_s]
+            self.label_to_as[ind] = a_s
 
         self.H_f = info['fixed_hubs']
         self.n_H = len(info['hubs'])
@@ -68,23 +69,29 @@ class SNDHLP:
             self.path[r_id] = []
             p_id = len(self.path[r_id])
             self.path[r_id].append(p_id)
-            path = list(r)
-            self.path_to_label[str(path)] = (r_id, p_id)
-            self.label_to_path[(r_id, p_id)] = path
+            path_ = list(r)
+            self.path_to_label[str(path_)] = (r_id, p_id)
+            self.label_to_path[(r_id, p_id)] = path_
 
         # paths that contain hub i in each request: {h_id:{r_id:[p_id, p_id], r_id:[...]...}, hub2:{...}....}
-        self.path_i = {hub_id: self.getPathByHub(self.path, hub_id) for hub_id in self.label_to_H.keys()}
+        self.path_i = {hub_id: self.getPathByHub(hub_id) for hub_id in self.label_to_H.keys()}
+        # pprint(self.path_i)
 
         # paths that contain arc: {arc_id:{r_id:[p_id, p_id], r_id:[...]...}, arc_id:{...}....}
 
-        self.path_a_t = {arc_t_id: self.getPathByArc(self.path, arc_t_id, arc_type="t") for arc_t_id in
+        self.path_a_t = {arc_t_id: self.getPathByArc(arc_t_id, arc_type="t") for arc_t_id in
                          self.label_to_at.keys()}  # rail arc
-        self.path_a_s = {arc_s_id: self.getPathByArc(self.path, arc_s_id, arc_type="s") for arc_s_id in
+
+        self.path_a_s = {arc_s_id: self.getPathByArc(arc_s_id, arc_type="s") for arc_s_id in
                          self.label_to_as.keys()}  # road arc
+
+        # self.path_a_t = {self.label_to_at[arc_t_id]: self.getPathByArc(self.path, arc_t_id, arc_type="t") for
+        # arc_t_id in self.label_to_at.keys()}  # rail arc self.path_a_s = {self.label_to_as[arc_s_id]:
+        # self.getPathByArc(self.path, arc_s_id, arc_type="s") for arc_s_id in self.label_to_as.keys()}  # road arc
         # self.path_arc = (lambda x, y: {**x, **y})(self.path_a_t, self.path_a_s)  # merge path information
 
         # num_path = [(od, x) for od, paths in self.path.items() for x in range(paths)]  # ind for path variables
-        self.y = self.model.addvars(list(self.label_to_path.keys()), lb=0.0, ub=1.0, vtype=GRB.CONTINUOUS, name="y")
+        self.y = self.model.addVars(list(self.label_to_path.keys()), lb=0.0, ub=1.0, vtype=GRB.CONTINUOUS, name="y")
         self.h = self.model.addVars(len(self.H), lb=0.0, ub=1, vtype=GRB.CONTINUOUS, name='h')
         self.t = self.model.addVars(len(self.A_t), lb=0.0, ub=GRB.INFINITY, vtype=GRB.CONTINUOUS, name="t")
         self.model.update()
@@ -94,7 +101,7 @@ class SNDHLP:
         self.c2 = self.model.addConstr(sum([x for _, x in self.h.items()]), GRB.LESS_EQUAL, self.n_H)
         self.c3 = self.model.addConstrs((self.y.sum(self.r_to_label[r], "*") == 1 for r in self.R.keys()))
 
-        self.c4 = self.model.addConstrs((sum([self.m[r_id] * self.y[p_id]
+        self.c4 = self.model.addConstrs((sum([self.m[self.label_to_r[r_id]] * self.y[r_id, p_id]
                                               for r_id in self.label_to_r.keys() for p_id in
                                               self.path_a_t[arc_t_id][r_id]])
                                          <= self.k_t * self.t[arc_t_id]
@@ -111,27 +118,31 @@ class SNDHLP:
         for arc_s_id, r_ids in self.path_a_s.items():
             arc_s = self.label_to_as[arc_s_id]
             l_a = self.A_s[arc_s]
-            for r_id, p_id_list in r_ids.itmes():
+            for r_id, p_id_list in r_ids.items():
                 r = self.label_to_r[r_id]
                 m_r = self.m[r]
                 for p_id in p_id_list:
-                    obj += self.c_s * l_a * m_r * self.y[p_id]
+                    obj += self.c_s * l_a * m_r * self.y[r_id,p_id]
 
-    def getPathByHub(self, path, hid):
-        res = {hid: {}}
+        self.model.setObjective(obj, GRB.MINIMIZE)
+        self.model.optimize()
+        print(self.model.getVars())
+
+    def getPathByHub(self, hid):
+        res = {}
         hub = self.label_to_H[hid]
-        for r_id, p_ids in path.items():
+        for r_id, p_ids in self.path.items():
             sel_p_id = []
             for p_id in p_ids:
-                path = self.label_to_path[p_id]
-                if hub in path:
+                p = self.label_to_path[r_id, p_id]
+                if hub in p:
                     sel_p_id.append(p_id)
-            res[hid][r_id] = sel_p_id
+            res[r_id] = sel_p_id
 
         return res
 
-    def getPathByArc(self, path, a_id, arc_type=None):
-        res = {a_id: {}}
+    def getPathByArc(self, a_id, arc_type=None):
+        res = {}
         if arc_type == "t":
             arc_map = self.label_to_at
         elif arc_type == "s":
@@ -140,17 +151,19 @@ class SNDHLP:
             raise Exception()
         arc = arc_map[a_id]
         start, end = arc
-        for r_id, p_ids in path.items():
+        for r_id, p_ids in self.path.items():
             sel_pid = []
             for p_id in p_ids:
-                path = self.label_to_path[p_id]
-                for ind, node in enumerate(path[:-1]):
-                    if path[ind] == start and path[ind + 1] == end:
+                p = self.label_to_path[r_id, p_id]
+                for ind, node in enumerate(p[:-1]):
+                    if p[ind] == start and p[ind + 1] == end:
                         sel_pid.append(p_id)
-            res[a_id][r_id] = sel_pid
+            res[r_id] = sel_pid
         return res
 
 
 if __name__ == "__main__":
     with open("info", "rb") as f:
-        Info = pickle.load("info")
+        Info = pickle.load(f)
+
+    sndhlp = SNDHLP(Info)
